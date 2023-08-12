4a387d8d773557f913b910f83c454e764561ee86
hsaagal78 <hsagal78@gmail.com>
2023-08-11 23:26:30 -0400

    folders and files are ready. File for password and requister are ready

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Landing(props) {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/notes')
+      .then(res => {
+        setNotes([...res.data.notes]);
+      });
+  }, []);
+
+  return (
+    <main className="landing">
+      <h1 className="text-center">Welcome to the future in note taking!</h1>
+
+      <h3>See what notes others are taking:</h3>
+
+      <div className="notes">
+        {!notes.length && <p>No notes have been added.</p>}
+
+        {notes.map(note => (
+          <div key={note._id} className="note column">
+            <h3>{note.text}</h3>
+            <div className="column">
+              <p>Added On: {note.createdAt}</p>
+              <p>Added By: {note.author.username}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  )
+}
+
+export default Landing;
